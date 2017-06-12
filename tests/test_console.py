bfd49e6763476b0e247b29aa63ee22f0251ba2da
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-06-12 01:21:15 +0000

    init commit for console unittest

---
+++ tests/test_console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import unittest
+import sys
+from console import HBNBCommand
+from unittest.mock import create_autospec
+
+
+class TestConsole(unittest.TestCase):
+    def setUp(self):
+        """ standard setUp """
+        self.mock_stdin = create_autospec(sys.stdin)
+        self.mock_stdout = create_autospec(sys.stdout)
+
+    def create(self, server=None):
+        """ create """
+        return HBNBCommand(stdin=self.mock_stdin, stdout=self.mock_stdout)
+
+    def test_exit(self):
+        """ test exit command """
+        cli = self.create()
+        self.assertTrue(cli.onecmd("quit"))
+        self.assertTrue(cli.onecmd("EOF"))
+
+    def _last_write(self, nr=None):
+        """:return: last `n` output lines"""
+        if nr is None:
+            return self.mock_stdout.write.call_args[0][0]
+        return "".join(map(lambda c: c[0][0],
+                           self.mock_stdout.write.call_args_list[-nr:]))
