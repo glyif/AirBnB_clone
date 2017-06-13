3086afacac790915f2372929c77adeb7eaee62ec
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-06-13 22:25:26 +0000

    fix indent

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ class User(BaseModel):
             super().__init__(args[0], kwargs)
         else:
             super().__init__()
-            self.email = ""
-            self.password = ""
-            self.first_name = ""
-            self.last_name = ""
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
